945c41a6589b1845cfbddc3d2be78d34ce42cb5f
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-06 12:53:50 +0600

    add sign in page

--- src/pages/authentication/signIn/SignIn.tsx
+++ src/pages/authentication/signIn/SignIn.tsx
@@ -1,18 +1,45 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { ReactNode } from "react";
+import tShirt from "../../../assets/images/tShirt/shirt-mockup-concept-with-plain-clothing.jpg";
 
-const SignIn = ({child}: {child: ReactNode}) => {
+const SignIn = ({ child }: { child: ReactNode }) => {
   return (
     <Dialog>
       <DialogTrigger className="flex">{child}</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent className="flex items-center max-w-[800px] sm:max-w-[900px] lg:max-w-[50%]">
+        {/* SignIn form */}
+        <div className="w-[50%] flex justify-center items-center">
+          <div>
+            <DialogHeader>
+              <DialogTitle>
+                <h1 className="text-3xl text-center">Sign In .</h1>
+              </DialogTitle>
+
+              <DialogDescription>
+                <p className="text-lg text-gray-600 mb-8">
+                  Sign in to your Laivaly account
+                </p>
+              </DialogDescription>
+            </DialogHeader>
+
+            {/* Login form */}
+            <div>
+                
+            </div>
+          </div>
+        </div>
+
+        {/* Image contetnt */}
+        <div className="rounded-lg overflow-hidden w-[50%]">
+          <img src={tShirt} alt="tShirt" />
+        </div>
       </DialogContent>
     </Dialog>
   );
