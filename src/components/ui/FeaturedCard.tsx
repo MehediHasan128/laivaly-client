b172352473f597dd33e030a6fe1f750164b84192
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-08 00:01:31 +0600

    complete feturedcard

--- src/components/ui/FeaturedCard.tsx
+++ src/components/ui/FeaturedCard.tsx
@@ -4,19 +4,29 @@ type TFeaturedCardProps = {
   description: string;
 };
 
-const FeaturedCard = ({ productImage, productCategory, description }: TFeaturedCardProps) => {
+const FeaturedCard = ({
+  productImage,
+  productCategory,
+  description,
+}: TFeaturedCardProps) => {
   return (
     <div className="relative rounded-2xl overflow-hidden group cursor-pointer">
-      <img className="w-full h-full" src={productImage} alt="" />
+      <img
+        className="w-full h-full grayscale-100 group-hover:grayscale-0 brightness-75 group-hover:brightness-100 group-hover:scale-110 duration-1000"
+        src={productImage}
+        alt=""
+      />
 
-      <div className="absolute inset-0 flex items-center justify-center bg-[#00000060] bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded">
-        <div className="transform translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-700">
-          <div className="text-2xl font-semibold text-white w-[70%] mx-auto text-center space-y-3">
-            <h1>{productCategory}</h1>
-            <p className="text-xs font-medium text-gray-200">
-              {description}
-            </p>
-            <button className="border border-[#5b8f70] text-sm font-medium px-3 py-1.5 rounded-full cursor-pointer hover:bg-[#5b8f70] hover:text-black hover:border-[#5b8f70] duration-500">Explore Collection</button>
+      <div className="absolute top-0 w-full h-full flex justify-center items-center text-3xl font-semibold text-white duration-1000">
+        <h1>{productCategory}</h1>
+      </div>
+      <div className="absolute top-[215px] inset-0 flex justify-center bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-1000 rounded">
+        <div className="transform translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-1000">
+          <div className="text-2xl font-semibold w-[70%] mx-auto text-center space-y-3 text-white">
+            <p className="text-sm font-semibold">{description}</p>
+            <button className="bg-[#31473A] text-sm font-medium px-3 py-1.5 rounded-full cursor-pointer  duration-500">
+              Explore Collection
+            </button>
           </div>
         </div>
       </div>
