dc8fd08c17e4607a3b9b95a85c16f267c87d1ee3
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-15 01:38:09 +0600

    add chart in admin dashboard

--- src/admin/adminDashboard.tsx/AdminDashboard.tsx
+++ src/admin/adminDashboard.tsx/AdminDashboard.tsx
@@ -1,3 +1,4 @@
+import ChartAreaInteractive from "./ChartAreaInteractive";
 import SectionCards from "./SectionCards";
 
 const AdminDashboard = () => {
@@ -6,7 +7,7 @@ const AdminDashboard = () => {
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <SectionCards />
-          <div className="px-4 lg:px-6">{/* <ChartAreaInteractive /> */}</div>
+          <div className="px-4 lg:px-6"><ChartAreaInteractive /></div>
         </div>
       </div>
     </div>
