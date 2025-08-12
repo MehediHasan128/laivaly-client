19d85091328db2183e971b2b45d7a1195d30cb41
MehediHasan128 <mehedihasan12926@gmail.com>
2025-08-12 14:15:58 +0600

    complete the login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,10 @@
+import LoginForm from "@/components/auth/LoginForm";
 import HorizontalDivider from "@/components/reusable/HorizontalDivider";
 import { smoochsans } from "@/styles/font";
-import { Eye } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { FaApple } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 
@@ -13,82 +15,85 @@ export const metadata: Metadata = {
 };
 
 const LoginPage = () => {
-
-
-
   return (
-    <main className="h-screen">
-      <div className="flex h-full">
-        {/* Image content */}
-        <div className="w-[50%]">
-          <div className="w-full h-full relative">
-            <Image
-              src="/images/categories/3.jpg"
-              alt="signInBanner"
-              fill
-              quality={100}
-              className="object-cover j"
-            />
+    <main className="h-screen flex">
+      {/* Main Container */}
+      <section className="w-full lg:w-[50%] xl:w-[40%]">
+        {/* Form Container */}
+        <div className="relative">
+          {/* Header Section */}
+          <div className="absolute w-full flex justify-center items-center gap-3 p-5">
+            <div className="relative size-10">
+              <Image
+                src="/images/logo/logo.png"
+                alt="logo"
+                quality={100}
+                fill
+              />
+            </div>
+            <h1 className={`${smoochsans.className} font-bold text-4xl`}>
+              Laivaly
+            </h1>
           </div>
-        </div>
 
-        <div className="w-[50%] h-full relative">
-          {/* Header section */}
-          <div className="absolute top-0 p-8">
-            <div className="flex items-center gap-3">
-              <div className="relative size-10">
-                <Image src="/images/logo/logo.png" alt="logo" fill />
-              </div>
-              <h1 className={`${smoochsans.className} text-3xl font-bold`}>
-                Laivaly
+          {/* Form Section */}
+          <div className="flex justify-center items-center h-screen">
+            <div className="text-center mt-5 md:mt-10">
+              <h1 className="uppercase text-4xl md:text-6xl lg:text-5xl xl:text-6xl font-light">
+                my laivaly account
               </h1>
-            </div>
-          </div>
 
-          {/* Main form section */}
-          <div className="border h-full flex justify-center items-center">
-            <div>
-              <div>
-                <h1 className="text-7xl font-extralight uppercase">
-                  my laivaly account
-                </h1>
-
-                <div className="mt-10 space-y-3">
-                  <button className="flex justify-center items-center gap-3 border rounded cursor-pointer active:scale-95 duration-500 text-xl w-96 mx-auto py-3 ">
-                    <FcGoogle className="text-2xl" />{" "}
-                    <span>Continue With Google</span>{" "}
-                  </button>
-                  <button className="flex justify-center items-center gap-3 border rounded cursor-pointer active:scale-95 duration-500 text-xl w-96 mx-auto py-3 ">
-                    <FaApple className="text-2xl" />{" "}
-                    <span>Continue With Apple</span>{" "}
-                  </button>
-                </div>
+              {/* Login in with google and apple id button */}
+              <div className="mt-5 md:mt-10 space-y-3">
+                {/* Google button */}
+                <button className="flex justify-center items-center gap-3 font-semibold cursor-pointer rounded border border-black mx-auto py-3 w-72 md:w-96 md:text-lg">
+                  <FcGoogle className="text-2xl md:text-3xl" />
+                  <span>Continue with Google</span>
+                </button>
+                {/* Apple button */}
+                <button className="flex justify-center items-center gap-3 font-semibold cursor-pointer rounded border border-black mx-auto py-3 w-72 md:w-96 md:text-lg">
+                  <FaApple className="text-2xl md:text-3xl" />
+                  <span>Continue with Apple</span>
+                </button>
               </div>
 
-              <HorizontalDivider title="OR" />
+              {/* Horizontal divider */}
+              <div className="my-10">
+                <HorizontalDivider title="OR" />
+              </div>
 
+              {/* Main form */}
               <div>
-                <h1 className="text-center uppercase font-medium text-3xl">
-                  continue with your email and password
+                <h1 className="uppercase font-medium text-xl">
+                  continue with your emai and password
                 </h1>
 
-                <div className="mt-10 w-96 mx-auto">
-                  <form className="space-y-3">
-                    <div>
-                      <input type="email" placeholder="Email *" className="w-full border rounded font-medium outline-none focus:border-black text-lg px-5 py-3" />
-                    </div>
-                    <div className="relative">
-                      <input type="password" placeholder="Password" className="w-full border rounded font-medium outline-none focus:border-black text-lg px-5 py-3" />
-
-                      <span className="absolute top-0 right-0 h-full rounded-r flex items-center cursor-pointer px-3"><Eye /></span>
-                    </div>
-                  </form>
+                <div className="mx-auto w-72 md:w-96 mt-10">
+                  <LoginForm />
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      {/* Image container */}
+      <section className="lg:w-[50%] xl:w-[60%] hidden lg:block">
+        <div className="relative h-full">
+          <Image
+            src="/images/categories/3.jpg"
+            alt="banner"
+            quality={100}
+            fill
+            className="object-cover object-center"
+          />
+
+          <div className="absolute top-0 size-full bg-gradient-to-t from-[#000000c3] to-transparent">
+            <div className={`${smoochsans.className} h-full flex items-end p-10`}>
+              <h1 className="text-white font-bold text-5xl xl:text-6xl"><span>Shop with confidence </span> <br /> <span className="text-7xl xl:text-9xl"><span className="text-8xl xl:text-[150px] font-extrabold">100%</span> purchase protection.</span></h1>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,9 +1,60 @@
+"use client";
+
+import { Eye, EyeClosed } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
 const LoginForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [showPass, setShowPass] = useState(false);
+
+  console.log(showPass);
+
+  return (
+    <form action="" className="space-y-3">
+      {/* email input */}
+      <div>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter laivaly email"
+          className="outline-none border focus:border-black w-full rounded font-medium px-5 py-3"
+        />
+      </div>
+      {/* password input */}
+      <div className="relative">
+        <input
+          type={showPass ? "text" : "password"}
+          name="password"
+          placeholder="Enter password"
+          className="outline-none border focus:border-black w-full rounded font-medium px-5 py-3"
+        />
+
+        {showPass ? (
+          <div
+            onClick={() => setShowPass(!showPass)}
+            className="absolute top-0 right-0 h-full rounded-r flex items-center cursor-pointer px-5"
+          >
+            <Eye className="size-5 md:size-6" />
+          </div>
+        ) : (
+          <div
+            onClick={() => setShowPass(!showPass)}
+            className="absolute top-0 right-0 h-full rounded-r flex items-center cursor-pointer px-5"
+          >
+            <EyeClosed className="size-5 md:size-6" />
+          </div>
+        )}
+
+        <h1 className="absolute right-0 font-medium p-0.5 hover:underline cursor-pointer">Forget Password</h1>
+      </div>
+      {/* Submit button */}
+      <div>
+        <button type="submit" className="border uppercase w-full bg-black text-white font-medium rounded active:scale-95 duration-500 cursor-pointer mt-5 lg:mt-10 py-3">login</button>
+      </div>
+
+      <h1>Don’t have an account? <Link href="/" className="font-semibold underline cursor-pointer">Sign up for free.</Link></h1>
+    </form>
+  );
 };
 
 export default LoginForm;
