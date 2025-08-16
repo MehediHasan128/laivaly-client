c037f83fdceac492ec64f297f6fa6b452fb2e12e
MehediHasan128 <mehedihasan12926@gmail.com>
2025-08-16 15:20:15 +0600

    complete the image serction of product details page

--- src/app/(pages)/products/[category]/[productId]/page.tsx
+++ src/app/(pages)/products/[category]/[productId]/page.tsx
@@ -1,42 +1,51 @@
 import Image from "next/image";
 
-interface TProductDetailsPageProps {
-  params: {
-    category: string;
-    productId: string;
-  };
-}
+const images = [
+  "/images/products/6.jpg",
+  "/images/products/6.jpg",
+  "/images/products/6.jpg",
+  "/images/products/6.jpg",
+];
 
-const ProductDetailsPage = ({ params }: TProductDetailsPageProps) => {
-  const { category, productId } = params;
-
+const ProductDetailsPage = () => {
   return (
     <main>
-      <section className="flex flex-col">
+      <section className="flex flex-col lg:flex-row">
         {/* Product image */}
-        <div>
-          <div className="relative h-[45vh]">
-            <Image src="/images/products/6.jpg" alt="" quality={100} fill className="object-cover" />
+        <div className="flex flex-col md:flex-row lg:flex-col gap-1.5 w-full h-[70vh] lg:h-[80vh] xl:h-[100vh] lg:w-[50%] xl:w-[50%]">
+          {/* Thumbnail Image */}
+          <div className="h-[80%] md:h-full md:w-[80%] lg:w-full lg:h-[80%]">
+            <div className="relative size-full">
+              <Image
+                src="/images/products/6.jpg"
+                alt="img"
+                quality={100}
+                fill
+                className="object-cover"
+              />
+            </div>
           </div>
 
-          <div className="flex gap-1.5 mt-1.5">
-            <div className="relative h-20 w-full">
-              <Image src="/images/products/6.jpg" alt="" quality={100} fill className="object-cover" />
-            </div>
-            <div className="relative h-20 w-full">
-              <Image src="/images/products/6.jpg" alt="" quality={100} fill className="object-cover" />
-            </div>
-            <div className="relative h-20 w-full">
-              <Image src="/images/products/6.jpg" alt="" quality={100} fill className="object-cover" />
-            </div>
-            <div className="relative h-20 w-full">
-              <Image src="/images/products/6.jpg" alt="" quality={100} fill className="object-cover" />
+          {/* All Images */}
+          <div className="h-[20%] md:h-full md:w-[20%] lg:w-full lg:h-[20%]">
+            <div className="h-full flex md:flex-col lg:flex-row gap-1.5">
+              {images.map((img, index) => (
+                <div key={index} className="relative size-full cursor-pointer">
+                  <Image
+                    src={img}
+                    alt="img"
+                    quality={100}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              ))}
             </div>
           </div>
         </div>
 
         {/* Product info */}
-        <div className=""></div>
+        <div className="lg:w-[50%] xl:w-[50%]"></div>
       </section>
     </main>
   );
