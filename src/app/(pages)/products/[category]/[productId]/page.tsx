2b6e009807aaece566fd384da89038cc57aac5c0
MehediHasan128 <mehedihasan12926@gmail.com>
2025-08-17 02:01:46 +0600

    male the color size and product quantity button functional

--- src/app/(pages)/products/[category]/[productId]/page.tsx
+++ src/app/(pages)/products/[category]/[productId]/page.tsx
@@ -1,3 +1,4 @@
+import ProductColorSizeAndQuantity from "@/components/productDetail/ProductColorSizeAndQuantity";
 import ProductDescription from "@/components/productDetail/ProductDescription";
 import ProductImages from "@/components/productDetail/ProductImages";
 import Button from "@/components/reusable/Button";
@@ -33,49 +34,7 @@ const ProductDetailsPage = () => {
           {/* Product Description */}
           <ProductDescription />
 
-          <div className="space-y-3">
-
-              {/* Color button */}
-              <div className="space-y-2">
-                <h1 className="font-semibold text-sm md:text-base">Select Color</h1>
-                <div className="flex gap-1.5">
-                  {
-                    colors.map((color, index) => (
-                      <div key={index} className="size-8 md:size-10 rounded-full overflow-hidden border-2 border-black p-0.5">
-                        <div className="size-full rounded-full" style={{backgroundColor: `${color}`}} />
-                      </div>
-                    ))
-                  }
-                </div>
-              </div>
-
-              {/* Color button */}
-              <div className="space-y-2">
-                <h1 className="font-semibold text-sm md:text-base">Select Size</h1>
-                <div className="flex flex-wrap gap-1.5">
-                  {
-                    sizes.map((size, index) => (
-                      <div key={index} className="border bg-accent rounded-full font-medium w-16 md:w-20 lg:w-16 xl:w-20 text-center text-sm xl:text-base py-1 md:py-1.5">
-                        {size}
-                      </div>
-                    ))
-                  }
-                </div>
-              </div>
-
-              {/* Product Quantity */}
-              <div className="space-y-2">
-                <h1 className="font-semibold text-sm md:text-base">Quantity</h1>
-                <div className="flex items-center w-fit rounded">
-                  <div className="bg-black text-white cursor-pointer active:scale-95 duration-700 px-2 py-1 rounded-l"><Minus /></div>
-                  <div className="text-xl font-semibold px-8">
-                    0
-                  </div>
-                  <div className="bg-black text-white cursor-pointer active:scale-95 duration-700 px-2 py-1 rounded-r"><Plus /></div>
-                </div>
-              </div>
-            
-          </div>
+          <ProductColorSizeAndQuantity colors={colors} sizes={sizes} />
 
 
           <div className="mt-10 flex gap-3">
---
+++ src/components/productDetail/ProductColorSizeAndQuantity.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Minus, Plus } from "lucide-react";
+import React, { useState } from "react";
+
+interface TProductColorSizeAndQuantityProps {
+  colors: string[];
+  sizes: string[];
+}
+
+const ProductColorSizeAndQuantity = ({
+  colors,
+  sizes,
+}: TProductColorSizeAndQuantityProps) => {
+  const [productColor, setProductColor] = useState<string | null>(null);
+  const [productSize, setProductSize] = useState<string | null>(null);
+  const [quantity, setQuantity] = useState<number>(0);
+
+  return (
+    <div className="space-y-3">
+      {/* Color button */}
+      <div className="space-y-2">
+        <h1 className="font-semibold text-sm md:text-base">Select Color</h1>
+        <div className="flex gap-1.5">
+          {colors.map((color, index) => (
+            <div
+              key={index}
+              onClick={() => setProductColor(color)}
+              className={`size-8 md:size-10 rounded-full overflow-hidden border-2 cursor-pointer ${
+                productColor === color ? "border-black" : "border-transparent"
+              } p-0.5`}
+            >
+              <div
+                className="size-full rounded-full"
+                style={{ backgroundColor: `${color}` }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Size button */}
+      <div className="space-y-2">
+        <h1 className="font-semibold text-sm md:text-base">Select Size</h1>
+        <div className="flex flex-wrap gap-1.5">
+          {sizes.map((size, index) => (
+            <div
+              key={index}
+              onClick={() => setProductSize(size)}
+              className={`border ${
+                productSize === size
+                  ? "bg-black text-white"
+                  : "bg-accent text-black"
+              } rounded-full font-medium w-16 md:w-20 lg:w-16 xl:w-20 text-center text-sm xl:text-base py-1 md:py-1.5 cursor-pointer`}
+            >
+              {size}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Product Quantity */}
+      <div className="space-y-2">
+        <h1 className="font-semibold text-sm md:text-base">Quantity</h1>
+        <div className="flex items-center w-fit rounded">
+          <div
+            onClick={() =>
+              setQuantity((quantity) => (quantity > 0 ? quantity - 1 : 0))
+            }
+            className="bg-black text-white cursor-pointer active:scale-95 duration-700 px-2 py-1 rounded-l"
+          >
+            <Minus />
+          </div>
+          <div className="text-xl font-semibold w-20 text-center">
+            {quantity}
+          </div>
+          <div
+            onClick={() => setQuantity(quantity + 1)}
+            className="bg-black text-white cursor-pointer active:scale-95 duration-700 px-2 py-1 rounded-r"
+          >
+            <Plus />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductColorSizeAndQuantity;
