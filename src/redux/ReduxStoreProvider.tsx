35e0b76920ff408dca992ab1b5d4ca184e2bbdd2
MehediHasan128 <mehedihasan12926@gmail.com>
2025-08-28 10:33:47 +0600

    setup redux-persit

--- src/redux/ReduxStoreProvider.tsx
+++ src/redux/ReduxStoreProvider.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import React, { ReactNode } from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import { Toaster } from 'sonner';
+import React, { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store";
+import { Toaster } from "sonner";
+import { PersistGate } from "redux-persist/integration/react";
 
-const ReduxStoreProvider = ({children}: {children: ReactNode}) => {
-    return (
-        <Provider store={store}>
-            {children}
-            <Toaster position="top-center" swipeDirections={["top"]} duration={3000} />
-        </Provider>
-    );
+const ReduxStoreProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+        <Toaster
+          position="top-center"
+          swipeDirections={["top"]}
+          duration={3000}
+        />
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default ReduxStoreProvider;
